4a2c7c3be5df5a60990d57ce78cfe1389e7e6bf8
Drake <drakedemuyt@gmail.com>
2020-03-01 18:28:32 -0500

    Adding functions to append buttons, add question text.

--- script.js
+++ script.js
@@ -23,14 +23,40 @@ var qa= {
 }
 
 var timeEl = document.querySelector("#timer")
-var quizDiv = document.querySelector(".class")
+var quizDiv = document.querySelector(".quiz")
 
 var timer = 100;
+var score = 0;
 
-// Start button function
 var start = document.querySelector("#start")
+var questionHeader = document.createElement("h2");
+var questionText = document.getElementsByTagName("h2");
+
+function addQuestion1(){
+    
+    quizDiv.appendChild(questionHeader);
+    questionText.textContent = qa.question1.question;
+    
+    for(var i = 0; i < 4; i++){
+      var buttons = document.createElement("button");
+      buttons.textContent = qa.question1.answers[i];
+      //
+      if(i + 1 == qa.question1.correct){
+        buttons.setAttribute("correct", true);
+      }
+    quizDiv.appendChild(buttons);
+
+    };
+    
+    
+
+  };
+// Start button function
 start.addEventListener("click", function(){
     setTime();
+    emptyDiv();
+    addQuestion1();
+    
     // Timer Interval Function (might need outside of start function)
     function setTime() {
         var timerInterval = setInterval(function() {
@@ -44,20 +70,25 @@ start.addEventListener("click", function(){
       
         }, 1000);
       }
-      
-    //   function sendMessage() {
-    //     timeEl.textContent = " ";
-      
-    //     var imgEl = document.createElement("img");
-      
-    //     imgEl.setAttribute("src", "images/image_1.jpg");
-    //     mainEl.appendChild(imgEl);
+    // Empty div
+    function emptyDiv(){
+      while(quizDiv.firstChild) {
+        quizDiv.removeChild(quizDiv.firstChild);
+        // addQuestion1();
+      }
+    }
+    
+    // Append question div
+      //For loop questions into buttons
+        //call click function
+        // on click event function()
+          //If correct == button id then score++ / Else timer - 10
+
       
     //   }
-      
-      setTime();
 
 });
+
 
 
 
