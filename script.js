e52228c3926871e2a273001be2392de3b46c4184
Drake <drakedemuyt@gmail.com>
2020-03-01 21:38:39 -0500

    Debugged functions to allow h2 to stay and created a getter function.

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
 // Arrays for Questions, Correct Answers, and Wrong Answers
-var qa= {
-    question1: {
+  var question1 = {
       question: "A?",
       answers : ["A", "B", "C", "D",],
       correct: 1,
-    },
-    question2: {
+    };
+   var question2 = {
       question: "B?",
       answers: ["A", "B", "C", "D",],
       correct: 2,
-    },
-    question3: {
+    };
+   var question3 = {
       question: "C?",
       answers: ["A", "B", "C", "D",],
       correct: 3,
-    },
-    question4: {
+    };
+   var question4 = {
       question: "D?",
       answers: ["A", "B", "C", "D",],
       correct: 4,
-    },
-}
+    };
 
-var timeEl = document.querySelector("#timer")
-var quizDiv = document.querySelector(".quiz")
+var timeEl = document.querySelector("#timer");
+var quizDiv = document.querySelector(".quiz");
 
 var timer = 100;
 var score = 0;
 
-var start = document.querySelector("#start")
+var start = document.querySelector("#start");
+var instructions = document.querySelector(".instructions");
 var questionHeader = document.createElement("h2");
-var questionText = document.getElementsByTagName("h2");
+var questionText = document.getElementById("question");
+var answerBtns = document.querySelector(".buttons")
 
-function addQuestion1(){
+// Getting Question
+function getQuestionInternal(selectedQuestion){
+  return questionText = selectedQuestion.question;
+}
+
+function addQuestion(selectedQuestion){
     
-    quizDiv.appendChild(questionHeader);
-    questionText.textContent = qa.question1.question;
-    
+    quizDiv.append(questionHeader);
+    questionText.innerHTML = selectedQuestion.question;
+
     for(var i = 0; i < 4; i++){
       var buttons = document.createElement("button");
-      buttons.textContent = qa.question1.answers[i];
-      //
-      if(i + 1 == qa.question1.correct){
+      buttons.textContent = selectedQuestion.answers[i];
+      buttons.className = "buttons";
+      //Setting tagging the correct answer
+      if(i + 1 == selectedQuestion.correct){
         buttons.setAttribute("correct", true);
       }
-    quizDiv.appendChild(buttons);
-
+    quizDiv.append(buttons);
     };
-    
-    
-
   };
 // Start button function
 start.addEventListener("click", function(){
     setTime();
     emptyDiv();
-    addQuestion1();
+    addQuestion(question1);
     
     // Timer Interval Function (might need outside of start function)
     function setTime() {
@@ -65,30 +67,37 @@ start.addEventListener("click", function(){
       
           if(timer === 0) {
             clearInterval(timerInterval);
-            sendMessage();
+            // sendMessage();
           }
       
         }, 1000);
       }
     // Empty div
     function emptyDiv(){
-      while(quizDiv.firstChild) {
-        quizDiv.removeChild(quizDiv.firstChild);
-        // addQuestion1();
+      while(quizDiv.lastChild !== questionText) {
+        quizDiv.removeChild(quizDiv.lastChild);
       }
     }
-    
+  });
+answerBtns.addEventListener("click", function(){
+
+  if (correct == true) {
+    score++;
+
+  }
+  else {
+    timer - 10;
+  }
+
+});
+
+
+// Answer Button Functions
+
     // Append question div
       //For loop questions into buttons
         //call click function
         // on click event function()
           //If correct == button id then score++ / Else timer - 10
-
-      
-    //   }
-
-});
-
-
 
 
